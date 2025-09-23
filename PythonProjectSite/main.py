07c6bc012ba32fa1084de9fce327c38e3c42dc3f
MarshalGW <136970742+Forever-proglab@users.noreply.github.com>
2025-09-23 20:56:33 +0500

    update

--- PythonProjectSite/main.py
+++ PythonProjectSite/main.py
@@ -1,6 +1,5 @@
 from flask import render_template
 from flask import Flask
-from flask_login import LoginManager
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'ght' # Замените на надежный ключ
