5061ec25724d72314ceff61681bf5fd6e60efb02
MarshalGW <136970742+Forever-proglab@users.noreply.github.com>
2025-09-29 18:52:01 +0500

    update

--- PythonProjectSite/main.py
+++ PythonProjectSite/main.py
@@ -1,10 +1,63 @@
-from flask import render_template, url_for
-from flask import Flask
+import re
+import bcrypt
+from kodland_db import db
+from random import randint
+from datetime import datetime, timedelta
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_register import *
+
+
+def hashed_password(plain_text_password):
+    # Мы добавляем "соль" к нашему пароль, чтобы сделать его декодирование невозможным
+    return bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+
+
+def check_password(plain_text_password, hashed_password):
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_password)
+
 
 app = Flask(__name__)
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+all_orders = []
+
+app.config.update(
+    SECRET_KEY='WOW SUCH SECRET'
+)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+
+@login_manager.user_loader
+def load_user(login):
+    return User(login)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        for key in request.form:
+            if request.form[key] == '':
+                return render_template('register.html', message='Все поля должны быть заполнены!')
+
+        row = db.users.get('login', request.form['login'])
+        if row:
+            return render_template('register.html', message='Такой пользователь уже существует!')
+
+        if request.form['password'] != request.form['password_check']:
+            return render_template('register.html', message='Пароли не совпадают')
+        data = dict(request.form)
+        data.pop('password_check')
+        db.users.put(data=data)
+        return render_template('register.html', message='Регистрация прошла успешно')
+    return render_template('register.html')
 
 
 @app.route('/')
@@ -25,6 +78,30 @@ def register():
 @app.route('/education')
 def education():
     return render_template('education.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        row = db.users.get('login', request.form['login'])
+        gh = db.users.get('email', request.form['email'])
+        if not row:
+            return render_template('login.html', error='Неправильный(ая) логин или пароль или почта')
+        if not gh:
+            return render_template('login.html', error='Неправильный(ая) логин или пароль или почта')
+        if request.form['password'] == row.password:
+            user = User(login)  # Создаем пользователя
+            login_user(user)  # Логинем пользователя
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', error='Неправильный(ая) логин или пароль или почта')
+    return render_template('login.html')
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return 'Пока'
 
 if __name__ == "__main__":
     app.run(debug=True)
