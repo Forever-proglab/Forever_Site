f7506d59e02d24fcd84984e9764769403b490360
MarshalGW <136970742+Forever-proglab@users.noreply.github.com>
2026-07-26 21:46:00 +0500

    Update main.py

--- PythonProjectSite/main.py
+++ PythonProjectSite/main.py
@@ -6,8 +6,10 @@ from random import randint
 from datetime import datetime, timedelta
 from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
-from flask_register import *
-from sqlalchemy import *
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 # def hashed_password(plain_text_password):
